7c325133736ff072a254736fd8102731174776f3
alumnog <alumnog@LG902_9.itson.edu.mx>
2022-10-13 08:50:27 -0700

    Se agrego comentario en la clase Main

---
+++ src/mx/itson/pato/entidades/Comentario.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.pato.entidades;
+
+import java.util.Date;
+
+/**
+ *
+ * @author alumnog
+ */
+public class Comentario {
+    
+    private Date fecha;
+    private String contenido;
+    private Usuario usuario;
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    
+    
+}
